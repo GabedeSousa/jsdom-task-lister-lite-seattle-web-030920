2c5540f8afe7cfe91e9e40e77c980e9aba94b126
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-04-21 14:29:30 -0700

    lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let ex = document.getElementById("create_task_form")
+  let tasks = document.getElementById("tasks")
+  ex.addEventListener("submit", function(e){
+    e.preventDefault()
+    let li = document.createElement("li")
+    li.innerHTML = document.getElementById("new_task_description").value
+    tasks.appendChild(li)
+  })
 });
