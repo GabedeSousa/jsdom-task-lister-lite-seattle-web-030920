2f24ad671262c888fb2b33249854ac71d1f2c3f3
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-06-02 11:35:54 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,67 +1,8 @@
 
 const element1 = document.getElementById("new_task_description")
-const apagar = document.getElementById("me-delete")
-const element2 =  document.getElementById("make-me-cool").innerHTML.value
+const element2 =  document.getElementById("make-me-cool")
 const task = document.getElementById("task")
 const element3 = document.createElement("li")
-
-// const  apaga = task.remove()
-
-
-function myFunc(){
-  element1.addEventListener("submit", function(e){
-    e.preventDefault()
-    task.appendChild("li")
-    
-    
-  })
-  
-}
-
-
-
-// function toDoDelete(element3) {
-//   $(element3).parents("li").remove();
-// }
-
-// function myDelete(){
-//   element3.addEventListener("click", toDoDelete(element3){
-
-// }
-
-myFunc()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Run code inside once all HTML is loaded
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
@@ -78,8 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // create a new list item <li></li>
     let li = document.createElement("li")
     // put the data from the form inside of our new li tag
-    li.innerHTML = document.getElementById("new_task_description").value
+    li.innerText = document.getElementById("new_task_description").value
     // add our new list item to our list
     tasks.appendChild(li)
   })
 });
+myFunc()
